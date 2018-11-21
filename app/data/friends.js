c0197fdc3fa6be6e1df749331492414bd8d32869
Daniel Murphy <tugs.murphy@yahoo.com>
2018-11-21 15:24:16 -0500

    added all pages and code...need to add readme

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,10 +2,69 @@
 
 var friendsArray = [
     {
-      customerName: "Tugs",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
+        "name":"Boris",
+        "photo":"https://images.pexels.com/photos/37547/suit-business-man-business-man-37547.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260.jpeg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        "name": "Clyde",
+        "photo":"https://images.pexels.com/photos/4618/man-person-hat-fur.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260.jpeg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "lillix",
+        "photo":"https://images.pexels.com/photos/371160/pexels-photo-371160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260.jpeg",
+        "scores":[
+            2,
+            4,
+            1,
+            2,
+            3,
+            3,
+            5,
+            1,
+            1,
+            5 
+        ]
+
+    },
+    {
+        "name": "Meatloaf",
+        "photo": "https://i.pinimg.com/236x/89/52/33/895233fb703b85c8b0f5bb8967eee354--meatloaf-musician-meatloaf-singer.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            5
+        ]
     }
   ];
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,16 +17,40 @@ module.exports = function(app) {
       res.json(friends);
     });
 
-    // app.post("/api/friends", function(req, res) {
-    //     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    //     // It will do this by sending out the value "true" have a table
-    //     // req.body is available since we're using the body parsing middleware
-         
-    //       friendsData.push(req.body);
-    //       res.json(true);
-        
-        
-    //   });
+    app.post("/api/friends", function(req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+         console.log(req.body)
+          // friendsData.push(req.body);
+          // res.json(true);
+        // set max difference of 50 because 5 * 10 = 50
+        // therefore the max difference can only be 50
+        var maxDiff = 50;
+        var matchedFriend = {};
+        var userData = req.body.scores;
+        // loop through all friends in friends.js
+        for (var i = 0; i < friends.length; i++) {
+          
+          var diff = 0;
+            // loop through the users scores from the survey.html inputs
+           for (var j = 0; j < userData.length; j++){
+            // get the total difference of the users score and each of the friends scores 
+            //  math.abs for absolute value
+                var totalDiff = Math.abs(userData[j]-friends[i].scores[j]);
+                diff += totalDiff;
+           }
+
+           if (diff < maxDiff){
+            maxDiff = diff;
+            matchedFriend = friends[i];
+            }
+
+        }
+
+        res.json(matchedFriend);
+
+      });
   
     
 };
